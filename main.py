ac0648dba1a84b2172579269a6d920cc2365efee
Debajyoti-R <debajyoti.r@myfbm.com>
2025-12-22 01:24:20 +0530

    Fix blob path formatting in upload_floorplan function

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ def upload_floorplan(plan_path, user_id, plan_id, project_id, credentials, index
     client = CloudStorageClient()
     bucket = client.bucket(credentials["CloudStorage"]["bucket_name"])
     if index:
-        blob_path = f"{project_id}.{plan_id}.{user_id}/{plan_path.name}.{index}"
+        blob_path = f"{project_id}/{plan_id}/{user_id}/{plan_path.name}.{index}"
     else:
-        blob_path = f"{project_id}.{plan_id}.{user_id}/{plan_path.name}"
+        blob_path = f"{project_id}/{plan_id}/{user_id}/{plan_path.name}"
     blob = bucket.blob(blob_path)
 
     blob.upload_from_filename(plan_path)
