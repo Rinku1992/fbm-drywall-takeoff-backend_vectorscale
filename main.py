65bd3e8bbeec682fadcbd09a407c4cecb0d7b058
Debajyoti-R <debajyoti.r@myfbm.com>
2025-12-22 04:15:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -509,7 +509,7 @@ async def generate_floorplan_upload_signed_URL(request: Request) -> str:
 
     client = CloudStorageClient()
     bucket = client.bucket(CREDENTIALS["CloudStorage"]["bucket_name"])
-    blob_path = f"{project_id}.{payload_plan.plan_id}.{user_id}/floor_plan.PDF"
+    blob_path = f"{project_id}/{payload_plan.plan_id}/{user_id}/floor_plan.PDF"
     blob = bucket.blob(blob_path)
     url = blob.generate_signed_url(
         version="v4",
