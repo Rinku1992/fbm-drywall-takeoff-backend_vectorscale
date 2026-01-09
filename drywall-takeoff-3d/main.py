3bb4b83b10b6932d66336295d14d74e2837330e3
Debajyoti-R <240895958+Debajyoti-R@users.noreply.github.com>
2026-01-09 17:54:41 +0530

    Update main.py

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -1067,8 +1067,11 @@ async def floorplan_to_3d(request: Request):
     hyperparameters = load_hyperparameters()
     floor_plan_modeller_3d = Extrapolate3D(hyperparameters)
     walls_3d, walls_3d_path = floor_plan_modeller_3d.extrapolate(model_2d_path=model_2d_path)
+    gltf_paths = floor_plan_modeller_3d.gltf(model_2d_path=model_2d_path)
     model_3d_path = floor_plan_modeller_3d.save_plot_3d(walls_3d_path)
     upload_floorplan(model_3d_path, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2))
+    for gltf_path in gltf_paths:
+        upload_floorplan(gltf_path, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2), directory="gltf")
     insert_model_3d(walls_3d, scale, index, plan_id, user_id, project_id, CREDENTIALS)
     logging.info("SYSTEM: A 3D Model of the Floorplan Generated Successfully")
 
