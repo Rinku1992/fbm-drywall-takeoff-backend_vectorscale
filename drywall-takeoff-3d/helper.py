1336233be243cd6e7372274dbe2a5171c75c1be8
Debajyoti-R <240895958+Debajyoti-R@users.noreply.github.com>
2026-01-14 18:59:22 +0530

    Create helper.py

---
+++ drywall-takeoff-3d/helper.py
@@ -0,0 +1,11 @@
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
+
+def load_vertex_ai_client(credentials, region="us-central1"):
+    with open(credentials["VertexAI"]["service_account_key"], 'r') as f:
+        project_id = json.load(f)["project_id"]
+    vertexai.init(project=project_id, location=region)
+    vertex_ai_client = GenerativeModel(credentials["VertexAI"]["llm"]["model_name"])
+    generation_config = credentials["VertexAI"]["llm"]["parameters"]
+    return vertex_ai_client, generation_config
