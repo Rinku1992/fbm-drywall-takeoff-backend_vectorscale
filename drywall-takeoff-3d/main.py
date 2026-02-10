da430509969d82aecd56e79c0746eee57a430462
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-10 23:07:28 +0530

    Update main.py

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -17,7 +17,7 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pydantic_core import ValidationError
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 import google.auth.transport.requests
 from google.oauth2.service_account import IDTokenCredentials
@@ -32,7 +32,15 @@ import math
 from preprocessing import preprocess
 from modeller_2d import FloorPlan2D
 from extrapolate_3d import Extrapolate3D
-from helper import load_vertex_ai_client, bigquery_run, transcribe, sha256
+from helper import (
+    load_vertex_ai_client,
+    bigquery_run,
+    transcribe,
+    sha256,
+    upload_floorplan,
+    insert_model_2d,
+    extract_floorplan_from_page,
+)
 
 
 def respond_with_UI_payload(payload, status_code=200):
@@ -41,30 +49,6 @@ def respond_with_UI_payload(payload, status_code=200):
         status_code=status_code,
         media_type="application/json",
     )
-
-
-def upload_floorplan(plan_path, user_id, plan_id, project_id, credentials, index=None, directory=None):
-    client = CloudStorageClient()
-    page_number = Path(plan_path.stem).suffix
-    if page_number:
-        blob_object_name = Path(str(plan_path).replace(page_number, '')).name
-    else:
-        blob_object_name = plan_path.name
-    bucket = client.bucket(credentials["CloudStorage"]["bucket_name"])
-    if directory:
-        if index:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{index}/{directory}/{blob_object_name}"
-        else:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{directory}/{blob_object_name}"
-    else:
-        if index:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{index}/{blob_object_name}"
-        else:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{blob_object_name}"
-    blob = bucket.blob(blob_path)
-
-    blob.upload_from_filename(plan_path)
-    return f"gs://{credentials["CloudStorage"]["bucket_name"]}/{blob_path}"
 
 
 def download_floorplan(user_id, plan_id, project_id, credentials, destination_path="/tmp/floor_plan.PDF"):
@@ -141,90 +125,6 @@ def insert_model_2d_revision(
             bigquery.ScalarQueryParameter("scale", "STRING", scale),
             bigquery.ScalarQueryParameter("model_2d", "JSON", model_2d),
             bigquery.ScalarQueryParameter("revision_number", "INT64", revision_number)
-        ]
-    )
-
-    query_output = bigquery_run(credentials, GBQ_query, job_config=job_config).result()
-    return query_output
-
-
-def insert_model_2d(
-    model_2d,
-    scale,
-    page_number,
-    plan_id,
-    user_id,
-    project_id,
-    GCS_URL_floorplan_page,
-    GCS_URL_target_drywalls_page,
-    credentials
-    ):
-    if not model_2d.get("metadata", None):
-        GBQ_query = f"SELECT model_2d FROM `drywall_takeoff.models` WHERE LOWER(project_id) = LOWER('{project_id}') AND LOWER(plan_id) = LOWER('{plan_id}') AND page_number = {page_number};"
-        query_output = bigquery_run(credentials, GBQ_query).result()
-        walls_2d_raw = list(query_output)[0].model_2d
-        walls_2d = json.loads(walls_2d_raw) if isinstance(walls_2d_raw, str) else walls_2d_raw
-        model_2d["metadata"] = walls_2d["metadata"]
-    GBQ_query = """
-    MERGE `drywall_takeoff.models` t
-    USING (
-        SELECT
-            @plan_id AS plan_id,
-            @project_id AS project_id,
-            @user_id AS user_id,
-            @page_number AS page_number,
-            @model_2d AS model_2d,
-            @source AS source,
-            @target_drywalls AS target_drywalls,
-            @scale AS scale,
-    ) s
-    ON LOWER(t.project_id) = LOWER(s.project_id) AND LOWER(t.plan_id) = LOWER(s.plan_id) AND t.page_number = s.page_number
-    WHEN MATCHED THEN
-    UPDATE SET
-        model_2d = s.model_2d,
-        scale = COALESCE(NULLIF(s.scale, ''), t.scale),
-        user_id = @user_id,
-        updated_at = CURRENT_TIMESTAMP()
-    WHEN NOT MATCHED THEN
-    INSERT (
-        plan_id,
-        project_id,
-        user_id,
-        page_number,
-        scale,
-        model_2d,
-        model_3d,
-        takeoff,
-        source,
-        target_drywalls,
-        created_at,
-        updated_at
-    )
-    VALUES (
-        s.plan_id,
-        s.project_id,
-        s.user_id,
-        s.page_number,
-        s.scale,
-        s.model_2d,
-        JSON '{}',
-        JSON '{}',
-        s.source,
-        s.target_drywalls,
-        CURRENT_TIMESTAMP(),
-        CURRENT_TIMESTAMP()
-    );
-    """
-    job_config = dict(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("plan_id", "STRING", plan_id),
-            bigquery.ScalarQueryParameter("project_id", "STRING", project_id),
-            bigquery.ScalarQueryParameter("user_id", "STRING", user_id),
-            bigquery.ScalarQueryParameter("page_number", "INT64", page_number),
-            bigquery.ScalarQueryParameter("scale", "STRING", scale),
-            bigquery.ScalarQueryParameter("model_2d", "JSON", model_2d),
-            bigquery.ScalarQueryParameter("source", "STRING", GCS_URL_floorplan_page),
-            bigquery.ScalarQueryParameter("target_drywalls", "STRING", GCS_URL_target_drywalls_page)
         ]
     )
 
@@ -878,83 +778,28 @@ async def floorplan_to_2d(request: Request):
     vertex_ai_client_partial = partial(vertex_ai_client.generate_content, generation_config=generation_config)
     floor_plan_modeller_2d = FloorPlan2D(hyperparameters, vertex_ai_client_partial)
     walls_2d_all = dict(pages=list())
-    for index, floor_plan_path in enumerate(floor_plan_paths_preprocessed):
-        floorplan_page_source = upload_floorplan(floor_plan_path, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2))
-        logging.info(f"SYSTEM: Preprocessed Floorplan Image uploaded to GCS from PAGE: {index}")
-        futures = dict()
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            futures["floorplan_to_walls"] = executor.submit(
-                floorplan_to_walls,
-                CREDENTIALS,
-                project_id,
-                plan_id,
-                user_id,
-                index
+    futures = list()
+    with ProcessPoolExecutor(max_workers=5) as executor:
+        for index, floor_plan_path in enumerate(floor_plan_paths_preprocessed):
+            futures.append(
+                executor.submit(
+                    extract_floorplan_from_page,
+                    CREDENTIALS,
+                    hyperparameters,
+                    user_id,
+                    project_id,
+                    plan_id,
+                    floor_plan_path,
+                    index,
+                    floor_plan_modeller_2d,
+                    floorplan_to_walls,
+                    transcribe
+                )
             )
-            futures["transcriber"] = executor.submit(
-                transcribe,
-                CREDENTIALS,
-                hyperparameters,
-                floor_plan_path,
-            )
-            wall_segmented_path = futures["floorplan_to_walls"].result()
-            upload_floorplan(wall_segmented_path, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2))
-            logging.info(f"SYSTEM: Wall Detection Completed from PAGE: {index}")
-
-            transcription_block_with_centroids, transcription_headers_and_footers = futures["transcriber"].result()
-            logging.info(f"SYSTEM: Transcription Completed from PAGE: {index}")
-
-        walls_2d, polygons, walls_2d_path, external_contour = floor_plan_modeller_2d.model(
-            image_path=wall_segmented_path,
-            floor_plan_path=floor_plan_path,
-            transcription_block_with_centroids=transcription_block_with_centroids,
-            transcription_headers_and_footers=transcription_headers_and_footers
-        )
-        floor_plan_modeller_2d.load_drywall_choices(walls_2d, polygons)
-        floor_plan_modeller_2d.load_ceiling_choices(polygons)
-        if not polygons:
-            continue
-        #if verbose.upper() == "TRUE":
-        model_2d_path = floor_plan_modeller_2d.save_plot_2d(walls_2d_path, floor_plan_path=floor_plan_path)
-        upload_floorplan(model_2d_path, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2))
-        #model_2d_path_overlay_enabled = floor_plan_modeller_2d.save_plot_2d(walls_2d_path, floor_plan_path=floor_plan_path, overlay_enabled=True)
-        #upload_floorplan(model_2d_path_overlay_enabled, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2))
-        floorplan_baseline, floorplan_page_statistics = floor_plan_modeller_2d.scale_to(floor_plan_path=floor_plan_path)
-        floorplan_baseline_page_source = upload_floorplan(floorplan_baseline, user_id, plan_id, project_id, CREDENTIALS, index=str(index).zfill(2))
-        logging.info(f"SYSTEM: A 2D Model of the Floorplan from PAGE: {index} Generated Successfully")
-        metadata = dict(
-            size_in_bytes=floorplan_page_statistics["size"],
-            height_in_pixels=floorplan_page_statistics["height"],
-            width_in_pixels=floorplan_page_statistics["width"],
-            origin=["LEFT", "TOP"],
-            offset=(0, 0),
-            contour_root_vertices=external_contour
-        )
-        insert_model_2d(
-            dict(walls_2d=walls_2d, polygons=polygons, metadata=metadata),
-            floor_plan_modeller_2d.scale,
-            index,
-            plan_id,
-            user_id,
-            project_id,
-            floorplan_page_source,
-            floorplan_baseline_page_source,
-            CREDENTIALS
-        )
-        page = dict(
-            plan_id=plan_id,
-            page_number=index,
-            size_in_bytes=floorplan_page_statistics["size"],
-            height_in_pixels=floorplan_page_statistics["height"],
-            width_in_pixels=floorplan_page_statistics["width"],
-            origin=["LEFT", "TOP"],
-            offset=(0, 0),
-            contour_root_vertices=external_contour,
-            scale=floor_plan_modeller_2d.scale,
-            walls_2d=walls_2d,
-            polygons=polygons,
-        )
-        walls_2d_all["pages"].append(page)
+        pages = [future.result() for future in futures]
+        for page in pages:
+            if page:
+                walls_2d_all["pages"].append(page)
     insert_plan(
         project_id,
         user_id,
@@ -988,7 +833,7 @@ async def load_latest_floorplan_to_2d(request: Request):
     bucket = client.bucket(CREDENTIALS["CloudStorage"]["bucket_name"])
     blob_path = f"tmp/{user_id.lower()}/model_2d.json"
     blob = bucket.blob(blob_path)
-    timeout = from_unix_epoch() + 120
+    timeout = from_unix_epoch() + 3600
     walls_2d_all = dict(pages=list())
     while from_unix_epoch() < timeout:
         try:
