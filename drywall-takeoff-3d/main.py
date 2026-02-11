07ce1d0451213ed8107cb1f7e51923b864fb1f61
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-12 04:47:25 +0530

    Update main.py

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -1165,6 +1165,8 @@ async def generate_drywall_overlaid_floorplan_download_signed_URL(request: Reque
 
     status = "IN PROGRESS"
     GBQ_query = f"SELECT pages FROM `{CREDENTIALS["GBQServer"]["table_name_plans"]}` WHERE LOWER(project_id) = LOWER('{project_id}') AND LOWER(plan_id) = LOWER('{plan_id}');"
+    if not list(bigquery_run(CREDENTIALS, GBQ_query).result()):
+        return respond_with_UI_payload(dict(error="Floor Plan already exists"))
     query_output = list(bigquery_run(CREDENTIALS, GBQ_query).result())[0]
     n_pages = query_output.pages
     timeout = from_unix_epoch() + (n_pages * 120)
