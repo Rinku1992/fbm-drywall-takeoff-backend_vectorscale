28060193673673816725f5936ba437eae251aa72
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-18 23:33:55 +0530

    Update extrapolate_3d.py

--- drywall-takeoff-3d/extrapolate_3d.py
+++ drywall-takeoff-3d/extrapolate_3d.py
@@ -2,7 +2,9 @@ from copy import deepcopy
 import json
 from pathlib import Path
 
+from pdf2image import convert_from_path
 import math
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -519,6 +521,29 @@ class Extrapolate3D(FloorPlan):
                 else:
                     continue
         return walls_3d, polygons
+
+    def recompute_dimensions_walls_and_polygons(self, walls_3d_JSON, polygons_JSON, pixel_aspect_ratio_new, floor_plan_pdf_path):
+        width, height = convert_from_path(floor_plan_pdf_path, dpi=400)[0].size
+        scale_x = 1920 / width
+        scale_y = 1080 / height
+        walls_3d_JSON_updated, polygons_JSON_updated = list(), list()
+        for wall_3d in walls_3d_JSON:
+            X1, Y1, X2, Y2 = wall_3d["wall_line"][0]['x'], wall_3d["wall_line"][0]['y'], wall_3d["wall_line"][1]['x'], wall_3d["wall_line"][1]['y']
+            X1_scaled, Y1_scaled, X2_scaled, Y2_scaled = scale_x * X1, scale_y * Y1, scale_x * X2, scale_y * Y2
+            length_target = round(math.hypot(
+                (X1_scaled - X2_scaled) * pixel_aspect_ratio_new["horizontal"],
+                (Y1_scaled - Y2_scaled) * pixel_aspect_ratio_new["vertical"]
+            ), 2)
+            wall_3d["length"] = length_target
+            walls_3d_JSON_updated.append(wall_3d)
+        for polygon in polygons_JSON:
+            polygon_vertices_scaled = [(scale_x * vertex[0], scale_y * vertex[1]) for vertex in polygon["vertices"]]
+            polygon = np.array(polygon_vertices_scaled)
+            area = cv2.contourArea(polygon)
+            polygon["area"] = scale_x * scale_y * area
+            polygons_JSON_updated.append(polygon)
+
+        return walls_3d_JSON_updated, polygons_JSON_updated
 
     def extrapolate(
         self,
