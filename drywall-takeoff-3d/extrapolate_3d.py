87de06cb415f1c71093904c9196b305447ec9fc7
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-19 01:21:01 +0530

    Update extrapolate_3d.py

--- drywall-takeoff-3d/extrapolate_3d.py
+++ drywall-takeoff-3d/extrapolate_3d.py
@@ -538,9 +538,9 @@ class Extrapolate3D(FloorPlan):
             walls_3d_JSON_updated.append(wall_3d)
         for polygon in polygons_JSON:
             polygon_vertices_scaled = [(scale_x * vertex[0], scale_y * vertex[1]) for vertex in polygon["vertices"]]
-            polygon = np.array(polygon_vertices_scaled)
+            polygon = np.array(polygon_vertices_scaled, np.float32)
             area = cv2.contourArea(polygon)
-            polygon["area"] = scale_x * scale_y * area
+            polygon["area"] = pixel_aspect_ratio_new["horizontal"] * pixel_aspect_ratio_new["vertical"] * area
             polygons_JSON_updated.append(polygon)
 
         return walls_3d_JSON_updated, polygons_JSON_updated
