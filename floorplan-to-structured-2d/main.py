a8cafa02d599e6baaadf3f3d1e49d93f9e582748
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-19 00:20:47 +0530

    Update main.py

--- floorplan-to-structured-2d/main.py
+++ floorplan-to-structured-2d/main.py
@@ -76,6 +76,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+bigquery_client = load_bigquery_client(CREDENTIALS)
 
 @app.post("/floorplan_to_structured_2d")
 async def floorplan_to_2d(request: Request):
@@ -153,7 +154,6 @@ async def floorplan_to_2d(request: Request):
                 contour_root_vertices=external_contour,
                 scales_architectural=floor_plan_modeller_2d.scales_architectural,
             )
-    bigquery_client = load_bigquery_client(CREDENTIALS)
     insert_model_2d(
         dict(walls_2d=walls_2d, polygons=polygons, metadata=metadata),
         floor_plan_modeller_2d.normalize_scale(floor_plan_modeller_2d.scale),
