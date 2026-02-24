7b8ce606ea874c3ff0c9646a1a189a846447d10d
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-25 03:39:03 +0530

    Update helper.py

--- floorplan-to-structured-2d/helper.py
+++ floorplan-to-structured-2d/helper.py
@@ -30,7 +30,7 @@ def transcribe(credentials, hyperparameters, floor_plan_path):
     transcriber = Transcriber(credentials, hyperparameters)
     return transcriber.transcribe(floor_plan_path, [0, 1, -1, -2])
 
-def upload_floorplan(plan_path, user_id, plan_id, project_id, credentials, index=None, directory=None):
+def upload_floorplan(plan_path, plan_id, project_id, credentials, index=None, directory=None):
     client = CloudStorageClient()
     page_number = Path(plan_path.stem).suffix
     if page_number:
@@ -40,14 +40,14 @@ def upload_floorplan(plan_path, user_id, plan_id, project_id, credentials, index
     bucket = client.bucket(credentials["CloudStorage"]["bucket_name"])
     if directory:
         if index:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{index}/{directory}/{blob_object_name}"
+            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{index}/{directory}/{blob_object_name}"
         else:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{directory}/{blob_object_name}"
+            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{directory}/{blob_object_name}"
     else:
         if index:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{index}/{blob_object_name}"
+            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{index}/{blob_object_name}"
         else:
-            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{blob_object_name}"
+            blob_path = f"{project_id.lower()}/{plan_id.lower()}/{blob_object_name}"
     blob = bucket.blob(blob_path)
 
     blob.upload_from_filename(plan_path)
@@ -75,10 +75,10 @@ def load_hyperparameters() -> dict:
 
     return hyperparameters
 
-def download_floorplan(user_id, plan_id, project_id, credentials, index, destination_path="/tmp/floor_plan_wall_processed.png"):
+def download_floorplan(plan_id, project_id, credentials, index, destination_path="/tmp/floor_plan_wall_processed.png"):
     client = CloudStorageClient()
     bucket = client.bucket(credentials["CloudStorage"]["bucket_name"])
-    blob_path = f"{project_id.lower()}/{plan_id.lower()}/{user_id.lower()}/{index}/floor_plan.png"
+    blob_path = f"{project_id.lower()}/{plan_id.lower()}/{index}/floor_plan.png"
     blob = bucket.blob(blob_path)
 
     destination_path = Path(destination_path)
