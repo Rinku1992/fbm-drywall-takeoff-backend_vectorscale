8028296426ff7f180058ca9f7821df76b40541cd
Debajyoti-R <debajyoti.r@myfbm.com>
2026-02-26 00:37:48 +0530

    Update main.py

--- floorplan-to-structured-2d/main.py
+++ floorplan-to-structured-2d/main.py
@@ -93,7 +93,7 @@ async def floorplan_to_2d(request: Request):
     verbose = parameters.get("verbose") or body.get("verbose")
     logging.info("SYSTEM: Received a Floorplan 2D Model Generation Request")
 
-    floor_plan_processed_path = download_floorplan(plan_id, project_id, CREDENTIALS, str(page_number).zfill(2))
+    floor_plan_processed_path = download_floorplan(user_id, plan_id, project_id, CREDENTIALS, str(page_number).zfill(2))
     logging.info(f"SYSTEM: Processed Floorplan Downloaded: Page Number: {page_number}")
 
     hyperparameters = load_hyperparameters()
