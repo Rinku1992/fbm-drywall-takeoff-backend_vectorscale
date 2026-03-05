e3b54bc766b0cd04e41951178a6250a59b13cbd6
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-05 17:57:31 +0530

    Update modeller_2d.py

--- floorplan-to-structured-2d/modeller_2d.py
+++ floorplan-to-structured-2d/modeller_2d.py
@@ -1619,7 +1619,7 @@ class FloorPlan2D(FloorPlan):
             if impute_drywall_disabled:
                 if not wall["polygons_drywall"][0]["enabled"] or not wall["polygons_drywall"][1]["enabled"]:
                     reference_line = [[wall["wall_line"][0]['x'], wall["wall_line"][0]['y'], wall["wall_line"][1]['x'], wall["wall_line"][1]['y']]]
-                    if self.is_inside_polygon((reference_line[0][0], reference_line[0][1]), polygon_vertices_external) and self.is_inside_polygon((reference_line[0][2], referene_line[0][3]), polygon_vertices_external):
+                    if self.is_inside_polygon((reference_line[0][0], reference_line[0][1]), polygon_vertices_external) and self.is_inside_polygon((reference_line[0][2], reference_line[0][3]), polygon_vertices_external):
                         target_lines = [[[wall_target["wall_line"][0]['x'], wall_target["wall_line"][0]['y'], wall_target["wall_line"][1]['x'], wall_target["wall_line"][1]['y']]] for wall_target in walls_2d[:]]
                         neighbors = self.nearest_neighbor(reference_line, 'A', target_lines, top_k=5)
                         valid_neighbor_found = False
