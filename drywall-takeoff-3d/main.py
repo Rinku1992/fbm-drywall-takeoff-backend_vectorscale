bd9676fad3638397315a06a0f221207afd89cd5d
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-07 23:16:30 +0530

    Update main.py

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -515,7 +515,7 @@ def insert_project(payload_project, bigquery_client, credentials):
     return created_at
 
 
-def floorplan_to_structured_2d(credentials, id_token, project_id, plan_id, user_id, page_number):
+def floorplan_to_structured_2d(credentials, id_token, project_id, plan_id, user_id, page_number, mask_factor):
     headers = {
         "Authorization": f"Bearer {id_token}",
         "Content-Type": "application/json"
@@ -527,7 +527,8 @@ def floorplan_to_structured_2d(credentials, id_token, project_id, plan_id, user_
             project_id=project_id,
             plan_id=plan_id,
             user_id=user_id,
-            page_number=page_number
+            page_number=page_number,
+            mask=mask_factor,
         )
     )
 
