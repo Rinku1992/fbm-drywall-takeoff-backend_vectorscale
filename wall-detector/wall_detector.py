b4cd167fc93026f6e1290bc620886e0187a7f930
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-08 06:49:45 +0530

    Update wall_detector.py

--- wall-detector/wall_detector.py
+++ wall-detector/wall_detector.py
@@ -50,8 +50,8 @@ class WallDetector:
             image = np.array(image_detected)
             mask_height_factor = mask_offset["vertical"]
             mask_width_factor = mask_offset["horizontal"]
-            image[:, -round(width_original * mask_width_factor):, :] = 255
-            image[-round(height_original * mask_height_factor):, :, :] = 255
+            image[:, -round(width_original * mask_width_factor):] = 255
+            image[-round(height_original * mask_height_factor):, :] = 255
             image_detected = Image.fromarray(image)
             image_detected = image_detected.resize((width, height))
         return image_detected
