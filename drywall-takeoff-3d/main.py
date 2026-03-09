853ce1cb7a0fd210719d98a068bf4f157dd4682e
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-10 02:15:04 +0530

    Update main.py

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -1297,7 +1297,7 @@ async def remove_floorplan(request: Request):
     GBQ_query = f"SELECT * FROM `drywall_takeoff.plans` WHERE LOWER(project_id) = LOWER('{project_id}') AND LOWER(plan_id) = LOWER('{plan_id}') AND LOWER(user_id) = LOWER('{user_id}');"
     query_output = list(bigquery_run(CREDENTIALS, bigquery_client, GBQ_query).result())
     if not query_output:
-        respond_with_UI_payload(dict(error="Plan ID: {} cannot be deleted".format(plan_id)))
+        return respond_with_UI_payload(dict(error="Plan ID: {} cannot be deleted".format(plan_id)))
     delete_floorplan(project_id, plan_id, user_id, bigquery_client, CREDENTIALS)
     logging.info("SYSTEM: Floorplan Deleted Successfully")
 
