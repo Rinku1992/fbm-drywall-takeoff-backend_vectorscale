d26e6e7bedf1fc7359bb41789afa19f778feb9e5
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-10 22:47:53 +0530

    Update prompt.py

--- drywall-takeoff-3d/prompt.py
+++ drywall-takeoff-3d/prompt.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 from pydantic import BaseModel
 
 
@@ -25,6 +25,9 @@ ARCHITECTURAL_DRAWING_CLASSIFIER = """
         - A page containing architecture plan will contain the architecture metadata information in text at the stray sections of the image, usually at the right and bottom section of the image. Generate a mask factor containing the information on the stray section of the image following the below instrution,
             - Generate the horizontal `mask_factor` (boundary -> [0, 1]) of the image by determining the fraction of the total width of the image that contains text information and isolated from the architecture drawing on the page on the right-most section of the page.
             - Generate the vertical `mask_factor` (boundary -> [0, 1]) of the image by determining the fraction of the total height of the image that contains text information and isolated from the architecture drawing on the page on the bottom-most section of the page.
+        - A page may contain one or more architecture drawings. Compute bounding box or visual grounding offset for each of the available drawings with offset containing `TOP-LEFT` and `BOTTOM-RIGHT` corner of the bounding box) and produce a list of bounding box offsets.
+            - REMEMBER, `TOPMOST-LEFTMOST` of the page is considered as the origin to compute the bounding box offset from.
+            - The offsets should be computed in fraction [0, 1] to represent the bounding box. If a `TOP-LEFT` of a bounding box lies in a point which is at a distance of 0.5 of the total width of the page from the origin in X-direction (towards `RIGHT`) and at a distance of 0.25 of the total height of the page from the origin in Y-direction (towards `DOWN`), then the `TOP-LEFT` offset of the bounding box should be (0.5, 0.25). Apply same rule to compute `BOTTOM-RIGHT` offset of a bounding box.
 
         Base your decision only on visual and textual evidence present in the drawing.
 
@@ -46,9 +49,15 @@ ARCHITECTURAL_DRAWING_CLASSIFIER = """
                     "horizontal": <mask factor for the width of the image in float rounded upto 2 decimal places>,
                     "vertical": <mask factor for the height of the image in float rounded upto 2 decimal places>
                 }}
+            "bounding_box_offsets":
+                [
+                    {{"offset_top_left": <`TOP-LEFT` offset of the bounding-box for architecture drawing 1>, "offset_bottom_right": <`BOTTOM-RIGHT` offset of the bounding-box for architecture drawing 1>}},
+                    {{"offset_top_left": <`TOP-LEFT` offset of the bounding-box for architecture drawing 2>, "offset_bottom_right": <`BOTTOM-RIGHT` offset of the bounding-box for architecture drawing 2>}}
+                ]
         }}
 """
 
 class ArchitecturalDrawingClassifierResponse(BaseModel):
     plan_type: str
     mask_factor: Dict
+    bounding_box_offsets: List[Dict]
