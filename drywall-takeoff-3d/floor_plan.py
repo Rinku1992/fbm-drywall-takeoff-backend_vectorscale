31663290e0373235190be85cbb98dd860775e5f2
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-19 03:40:32 +0530

    Update floor_plan.py

--- drywall-takeoff-3d/floor_plan.py
+++ drywall-takeoff-3d/floor_plan.py
@@ -26,8 +26,8 @@ class FloorPlan:
 
     def compute_pixel_aspect_ratio(self, scale_new, pixel_aspect_ratio_standard):
         scale_new_unnormalized = f"{round(float(Fraction(scale_new.split('=')[0].strip('`'))), 2)}``:1`0``"
-        scale_new_on_paper_length = float(scale_new_unnormalized.split(':')[0].strip('`'))
-        scale_new_real_world_length_in_feet_and_inches_ = scale_new_unnormalized.split(':')[1].strip('`').split('`')
+        scale_new_on_paper_length = float(scale_new_unnormalized.split(':')[0].strip('`"'))
+        scale_new_real_world_length_in_feet_and_inches_ = scale_new_unnormalized.split(':')[1].strip('`"')
         if scale_new_real_world_length_in_feet_and_inches_.find('`') !=-1:
             scale_new_real_world_length_in_feet_and_inches = scale_new_real_world_length_in_feet_and_inches_.split('`')
         else:
