7532dfd4b6198b367188dd75f499218002482d92
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-19 02:54:50 +0530

    Update floor_plan.py

--- floorplan-to-structured-2d/floor_plan.py
+++ floorplan-to-structured-2d/floor_plan.py
@@ -52,7 +52,11 @@ class FloorPlan:
 
     def compute_pixel_aspect_ratio(self, scale_new, pixel_aspect_ratio_standard):
         scale_new_on_paper_length = float(scale_new.split(':')[0].strip('`'))
-        scale_new_real_world_length_in_feet_and_inches = scale_new.split(':')[1].strip('`').split('`')
+        scale_new_real_world_length_in_feet_and_inches_ = scale_new.split(':')[1].strip('`"')
+        if scale_new_real_world_length_in_feet_and_inches_.find('`') !=-1:
+            scale_new_real_world_length_in_feet_and_inches = scale_new_real_world_length_in_feet_and_inches_.split('`')
+        else:
+            scale_new_real_world_length_in_feet_and_inches = scale_new_real_world_length_in_feet_and_inches_.split("'")
         scale_new_real_world_length_in_feet = float(scale_new_real_world_length_in_feet_and_inches[0])
         if len(scale_new_real_world_length_in_feet_and_inches) == 2:
             scale_new_real_world_length_in_feet += float(scale_new_real_world_length_in_feet_and_inches[-1]) / 12
