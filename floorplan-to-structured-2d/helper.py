fc3fafa934f7467920aa7796b197ebc7801405ec
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-20 06:16:10 +0530

    Update helper.py

--- floorplan-to-structured-2d/helper.py
+++ floorplan-to-structured-2d/helper.py
@@ -292,14 +292,10 @@ def phoenix_call(generate_content_lambda, max_retry=5, base_delay=1.0, pydantic_
 def load_section_from_page(wall_segmented_path, floor_plan_path, bounding_box_offset, section_name):
     offset_top_left_X, offset_top_left_Y = bounding_box_offset["offset_top_left"]
     offset_bottom_right_X, offset_bottom_right_Y = bounding_box_offset["offset_bottom_right"]
-    if offset_top_left_X > 0.01:
-        offset_top_left_X -= 0.01
-    if offset_top_left_Y > 0.01:
-        offset_top_left_Y -= 0.01
-    if offset_bottom_right_X > 0.01:
-        offset_bottom_right_X += 0.01
-    if offset_bottom_right_Y > 0.01:
-        offset_bottom_right_Y += 0.01
+    offset_top_left_X = max(offset_top_left_X - 0.05, 0)
+    offset_top_left_Y = max(offset_top_left_Y - 0.05, 0)
+    offset_bottom_right_X  = min(offset_bottom_right_X + 0.05, 1)
+    offset_bottom_right_Y  = min(offset_bottom_right_Y + 0.05, 1)
     canvas = Image.open(wall_segmented_path)
     canvas = canvas.convert("RGB")
     width_in_pixels, height_in_pixels = canvas.size
