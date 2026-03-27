41896ed6ee336738f1184e07dc2b7599ebf56a2f
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-27 19:57:24 +0530

    Update prompt.py

--- floorplan-to-structured-2d/prompt.py
+++ floorplan-to-structured-2d/prompt.py
@@ -554,3 +554,17 @@ CEILING_CHOICES = [
     "Angled-Plane",
     "Boxed-Beam"
 ]
+
+WALL_CHOICES = [
+  {"wall_type": "OPEN_TO_BELOW", "is_height_static": False},
+  {"wall_type": "FULL_WALL", "is_height_static": True},
+  {"wall_type": "HALF_WALL", "is_height_static": False},
+  {"wall_type": "STAIRCASE_WALL", "is_height_static": False},
+  {"wall_type": "SOFFITS", "is_height_static": False},
+  {"wall_type": "MULTI_FLOOR_ALIGNMENT", "is_height_static": False},
+  {"wall_type": "DEMISING_WALL", "is_height_static": True},
+  {"wall_type": "GARAGE_SEPARATION_WALL", "is_height_static": True},
+  {"wall_type": "SHAFT_WALL", "is_height_static": False},
+  {"wall_type": "WET_WALL", "is_height_static": False},
+  {"wall_type": "HALLWAY_WALL", "is_height_static": True} 
+]
