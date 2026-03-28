a62a074439bfcf291f4ab2b769102360e7e90fe5
Debajyoti-R <debajyoti.r@myfbm.com>
2026-03-29 03:21:48 +0530

    Update main.py

--- floorplan-to-structured-2d/main.py
+++ floorplan-to-structured-2d/main.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 import json
 import requests
+from time import sleep
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -37,7 +38,7 @@ def respond_with_UI_payload(payload, status_code=200):
     )
 
 
-def floorplan_to_walls(credentials, project_id, plan_id, user_id, page_number, mask, output_path=None):
+def floorplan_to_walls(credentials, project_id, plan_id, user_id, page_number, mask, output_path=None, max_retry=5):
     auth_req = google.auth.transport.requests.Request()
     service_account_credentials = IDTokenCredentials.from_service_account_file(
         credentials["service_compute_account_key"],
@@ -51,17 +52,22 @@ def floorplan_to_walls(credentials, project_id, plan_id, user_id, page_number, m
         "Content-Type": "application/json"
     }
 
-    response = requests.post(
-        f"{credentials["CloudRun"]["APIs"]["wall_detector"]}/detect_wall",
-        headers=headers,
-        json=dict(
-            project_id=project_id,
-            plan_id=plan_id,
-            user_id=user_id,
-            page_number=page_number,
-            mask=mask
+    for _ in range(max_retry):
+        response = requests.post(
+            f"{credentials["CloudRun"]["APIs"]["wall_detector"]}/detect_wall",
+            headers=headers,
+            json=dict(
+                project_id=project_id,
+                plan_id=plan_id,
+                user_id=user_id,
+                page_number=page_number,
+                mask=mask
+            )
         )
-    )
+        if response.status_code == 200:
+            break
+        else:
+            sleep(30)
 
     if not output_path:
         output_path  = Path("/tmp/floor_plan_wall_segmented.png")
