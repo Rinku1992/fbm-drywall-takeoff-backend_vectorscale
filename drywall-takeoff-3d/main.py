cc2f27f12be8ab1f05f80653d9748c09c61f39ff
Debajyoti-R <debajyoti.r@myfbm.com>
2026-04-01 04:49:53 +0530

    Update main.py

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -821,6 +821,8 @@ async def floorplan_to_2d(request: Request):
     try:
         with ThreadPoolExecutor(max_workers=20) as executor:
             for page_number in range(n_pages):
+                if page_number != 0 and page_number % 40 == 0:
+                    sleep(120)
                 id_token = load_floorplan_to_structured_2d_ID_token(CREDENTIALS)
                 executor.submit(
                     floorplan_to_structured_2d,
