465d36b71dbd044c05e8417809a0468bc62b84b2
Debajyoti-R <debajyoti.r@myfbm.com>
2026-04-06 04:28:40 +0530

    Update main.py

--- floorplan-to-structured-2d/main.py
+++ floorplan-to-structured-2d/main.py
@@ -129,7 +129,7 @@ def page_to_structured_2d(
     walls_2d, polygons, _, external_contour = floor_plan_modeller_2d.model(
         bounding_box_offset_marginalized,
         image_path=wall_segmented_sectioned_path,
-        model_2d_path=f"/tmp/{project_id}/{plan_id}/{user_id}/walls_2d_{str(page_number).zfill(4)}_{str(page_section_number)}.json",
+        model_2d_path=f"/tmp/{project_id}/{plan_id}/{user_id}/walls_2d_{str(page_number).zfill(4)}_{str(page_section_number).replace('/', '_')}.json",
         floor_plan_path=floor_plan_processed_path,
         transcription_block_with_centroids=transcription_block_with_centroids,
         transcription_headers_and_footers=transcription_headers_and_footers,
