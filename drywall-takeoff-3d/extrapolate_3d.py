b7bfe182eb842e1076b3199c9f5ae70b372b4f78
Debajyoti-R <debajyoti.r@myfbm.com>
2026-04-09 01:02:40 +0530

    Update extrapolate_3d.py

--- drywall-takeoff-3d/extrapolate_3d.py
+++ drywall-takeoff-3d/extrapolate_3d.py
@@ -273,6 +273,7 @@ class Extrapolate3D(FloorPlan):
             thickness=wall_line["thickness"],
             height=wall_line["height"],
             length=wall_line["length"],
+            openings=wall_line["openings"],
             type=wall_line["type"],
             surfaces_drywall=list(),
             wall_line=wall_line["wall_line"],
