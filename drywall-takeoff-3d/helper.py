a44bc2ac10c5edec21d78a99dd25ad53b9552e14
Debajyoti-R <debajyoti.r@myfbm.com>
2026-04-24 01:32:31 +0530

    Update helper.py

--- drywall-takeoff-3d/helper.py
+++ drywall-takeoff-3d/helper.py
@@ -3,14 +3,33 @@ from json.decoder import JSONDecodeError
 import logging
 import hashlib
 from pathlib import Path
+import datetime
+from time import sleep
+from pypdf import PdfReader, PdfWriter
+from io import BytesIO
 
+import math
+import random
+random.seed(0)
+
+import geoip2.database as geoip2_database
 from google.cloud import bigquery
 from google.cloud.storage import Client as CloudStorageClient
 import google.auth.transport.requests
 from google.oauth2.service_account import IDTokenCredentials
 from google.oauth2 import service_account
 from google.cloud.pubsub_v1 import SubscriberClient
-from google.api_core.exceptions import DeadlineExceeded
+from google.api_core.exceptions import ResourceExhausted, ServiceUnavailable, DeadlineExceeded
+import vertexai
+from vertexai.generative_models import GenerativeModel
+from vertexai.generative_models import Content, Part
+from vertexai.caching import CachedContent
+
+from prompts import (
+    FLOORPLAN_TO_MULTIPAGE_ELEVATION_MAPPER,
+    FloorplanToMultipageElevationMapperResponse,
+    FEEDBACK_GENERATOR
+)
 
 
 def load_bigquery_client(credentials):
@@ -237,3 +256,146 @@ def query_subscriber_messages(credentials, subscriber_client, queries):
         except JSONDecodeError:
             continue
     return False, acknowledged_queries
+
+def load_vertex_ai_client(credentials, ip_address, prompts=None, default_region="us-central1"):
+    with open(credentials["VertexAI"]["service_account_key"], 'r') as f:
+        project_id = json.load(f)["project_id"]
+    region = load_nearest_region(
+        ip_address,
+        credentials["geolite_database"],
+        credentials["VertexAI"]["llm"]["available_regions"],
+        default_region=default_region
+    )
+    vertexai.init(project=project_id, location=region)
+    vertex_ai_client = lambda system_instruction: GenerativeModel(
+        credentials["VertexAI"]["llm"]["model_name"],
+        system_instruction=system_instruction
+    )
+    is_cached = False
+    if prompts and GenerativeModel(credentials["VertexAI"]["llm"]["model_name"]).count_tokens(prompts).total_tokens >= 1024:
+        is_cached = True
+        cached_content = CachedContent.create(
+            model_name=credentials["VertexAI"]["llm"]["model_name"],
+            contents=prompts,
+            ttl=datetime.timedelta(minutes=60),
+            display_name="drywall_predictor_cache"
+        )
+        vertex_ai_client = GenerativeModel.from_cached_content(cached_content)
+    generation_config = credentials["VertexAI"]["llm"]["parameters"]
+    return vertex_ai_client, generation_config, is_cached
+
+def load_nearest_region(ip_address, geolite_database, available_regions, default_region="us-central1"):
+    def _compute_haversine_distance(latitude_1, longitude_1, latitude_2, longitude_2):
+        R = 6371
+        d_latitude = math.radians(latitude_2-latitude_1)
+        d_longitude = math.radians(longitude_2-longitude_1)
+        a = math.sin(d_latitude/2)**2 + math.cos(math.radians(latitude_1)) * math.cos(math.radians(latitude_2)) * math.sin(d_longitude/2)**2
+        return 2*R*math.asin(math.sqrt(a))
+
+    if not ip_address or "," not in ip_address:
+        return default_region
+    if ip_address and "," in ip_address:
+        ip_address = ip_address.split(",")[0].strip()
+    geoip2_reader = geoip2_database.Reader(geolite_database)
+    try:
+        response = geoip2_reader.city(ip_address)
+        (response.location.latitude, response.location.longitude, response.country.iso_code)
+        nearest_region = None
+        minimum_distance = float("inf")
+        for region, (latitude, longitude) in available_regions.items():
+            distance = _compute_haversine_distance(response.location.latitude, response.location.longitude, latitude, longitude)
+            if distance < minimum_distance:
+                minimum_distance = distance
+                nearest_region = region
+        return nearest_region
+    except Exception:
+        return default_region
+
+def phoenix_call(generate_content_lambda, max_retry=5, base_delay=1.0, pydantic_model=None, verify_field_counts=None):
+    n_iterations = 0
+    temperature = 0
+    exceptions = list()
+    feedback_prompt = ''
+    while n_iterations < max_retry:
+        try:
+            response = generate_content_lambda(feedback_prompt, temperature)
+            if pydantic_model:
+                json_response = json.loads(response.text.strip("`json").replace("{{", '{').replace("}}", '}'))
+                if verify_field_counts:
+                    for field, count in verify_field_counts.items():
+                        if len(json_response[field]) != count:
+                            raise ValueError(f"Predicted {field} count: {len(json_response[field])} does not match with the expected number: {count}")
+                response_json_pydantic = pydantic_model(**json_response)
+                return response_json_pydantic, json_response
+            return response.text
+        except (ResourceExhausted, ServiceUnavailable, DeadlineExceeded) as e:
+            n_iterations += 1
+            if n_iterations >= max_retry:
+                raise e
+            sleep_time = base_delay * (2 ** (n_iterations - 1)) + random.uniform(0, 0.5)
+            sleep(sleep_time)
+            logging.warning(f"SYSTEM: {e}: RETRYING ...")
+        except Exception as e:
+            n_iterations += 1
+            if n_iterations >= max_retry:
+                raise e
+            exceptions.append(e)
+            system_feedback = [Part.from_text(FEEDBACK_GENERATOR.format(max_retry=max_retry, exceptions=exceptions))]
+            feedback_prompt = Content(role="model", parts=system_feedback)
+            temperature = min(0.5 * (n_iterations + 1) / max_retry, 0.5)
+            logging.warning(f"SYSTEM: Response Generation/Parsing failed with ERROR: {e}")
+            logging.warning(f"SYSTEM: RETRYING with TEMPERATURE: {temperature}")
+
+def map_floorplan_to_multipage_elevation(credentials, client_ip_address, pdf_path):
+    vertex_ai_client, vertex_ai_generation_config, is_cached = load_vertex_ai_client(
+        credentials,
+        client_ip_address,
+        prompts=[FLOORPLAN_TO_MULTIPAGE_ELEVATION_MAPPER]
+    )
+    with open(pdf_path, "rb") as f:
+        bytes_pdf = f.read()
+    reader = PdfReader(BytesIO(bytes_pdf))
+    pages = list()
+
+    for index, page in enumerate(reader.pages):
+        writer = PdfWriter()
+        writer.add_page(page)
+
+        buffer = BytesIO()
+        writer.write(buffer)
+
+        pages.append({
+            "page_number": index,
+            "bytes": buffer.getvalue()
+        })
+
+    parts = list()
+    for page in pages:
+        parts.append(Part.from_text(f"PAGE: {page["page_number"]}"))
+        parts.append(Part.from_data(page["bytes"], mime_type="application/pdf"))
+    query = Content(role="user", parts=parts)
+
+    try:
+        if is_cached:
+            _, elevation_map = phoenix_call(
+                lambda feedback_prompt, temperature: vertex_ai_client.generate_content(
+                    contents=[feedback_prompt, query] if feedback_prompt else [query],
+                    generation_config={**vertex_ai_generation_config, "temperature": temperature},
+                ),
+                max_retry=credentials["VertexAI"]["llm"]["max_retry"],
+                pydantic_model=FloorplanToMultipageElevationMapperResponse,
+            )
+        else:
+            _, elevation_map = phoenix_call(
+                lambda feedback_prompt, temperature: vertex_ai_client(FLOORPLAN_TO_MULTIPAGE_ELEVATION_MAPPER).generate_content(
+                    contents=[feedback_prompt, query] if feedback_prompt else [query],
+                    generation_config={**vertex_ai_generation_config, "temperature": temperature},
+                ),
+                max_retry=credentials["VertexAI"]["llm"]["max_retry"],
+                pydantic_model=FloorplanToMultipageElevationMapperResponse,
+            )
+    except Exception as e:
+        logging.warning(f"SYSTEM: Floor Plan to Multi-page Elevation mapping has failed: {e}")
+        elevation_map = dict()
+
+    return elevation_map
