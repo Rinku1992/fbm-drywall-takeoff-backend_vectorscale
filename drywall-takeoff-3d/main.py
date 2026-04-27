9caca41f1e2652b06324d7357be860e214fc24ab
Debajyoti-R <debajyoti.r@myfbm.com>
2026-04-27 10:49:28 -0700

    Extract wall coordinates from 3D JSON structure

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -1390,6 +1390,7 @@ async def compute_takeoff(request: Request):
     )
     for wall in walls_3d_JSON:
         drywall_count = 0
+        X1, Y1, X2, Y2 = wall["wall_line"][0]['x'], wall["wall_line"][0]['y'], wall["wall_line"][1]['x'], wall["wall_line"][1]['y']
         for drywall in wall["surfaces_drywall"]:
             drywall_height_A = floor_plan_modeller_3d.extrapolate_drywall_height_given_polygon(
                     X1,
