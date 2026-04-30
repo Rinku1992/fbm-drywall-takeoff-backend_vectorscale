6eada94091e465c97685e021dae5299f83dc38fb
Debajyoti-R <debajyoti.r@myfbm.com>
2026-04-30 14:28:41 +0530

    Update preprocessing.py

--- floorplan-to-structured-2d/preprocessing.py
+++ floorplan-to-structured-2d/preprocessing.py
@@ -31,7 +31,7 @@ def to_sharp(image_path_page):
     cv2.imwrite(output_path, sharpened)
     return sharpened
 
-def preprocess(pdf_path, page_index, dpi, image_path="/tmp/floor_plan.png"):
+def preprocess(pdf_path, page_index, dpi=400, image_path="/tmp/floor_plan.png"):
     image_path = Path(image_path)
     image_path_page = image_path.parent.joinpath(image_path.stem).with_suffix(f".{str(page_index).zfill(2)}{image_path.suffix}")
     process_page(pdf_path, page_index, image_path_page, dpi)
