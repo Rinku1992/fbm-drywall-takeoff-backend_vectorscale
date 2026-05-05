ccb53079a0df9bf3fd0af4095d56c0608a3130bd
Debajyoti-R <debajyoti.r@myfbm.com>
2026-05-05 07:59:31 +0530

    Fix slope assignment in modeller_2d.py

--- floorplan-to-structured-2d/modeller_2d.py
+++ floorplan-to-structured-2d/modeller_2d.py
@@ -1626,7 +1626,7 @@ class FloorPlan2D(FloorPlan):
             vertices=vertices,
             type=model_polygon["ceiling"]["ceiling_type"],
             height=model_polygon["ceiling"]["height"] if model_polygon["ceiling"]["height"] else height_default,
-            slope=slope=math.degrees(math.atan2(model_polygon["ceiling"]["pitch"]["rise"], model_polygon["ceiling"]["pitch"]["run"])),
+            slope=math.degrees(math.atan2(model_polygon["ceiling"]["pitch"]["rise"], model_polygon["ceiling"]["pitch"]["run"])),
             slope_enabled=model_polygon["ceiling"]["slope_enabled"],
             tilt_axis=model_polygon["ceiling"]["tilt_axis"],
             room_name=model_polygon["ceiling"]["room_name"],
