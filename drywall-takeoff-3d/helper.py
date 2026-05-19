cd64063db6fefdaf125496538e83c98c595c2d46
Debajyoti-R <debajyoti.r@myfbm.com>
2026-05-19 06:15:49 -0700

    Rename function to load_drywall_weights with 2D input

--- drywall-takeoff-3d/helper.py
+++ drywall-takeoff-3d/helper.py
@@ -719,12 +719,12 @@ def insert_page(
     query_output = bigquery_run(credentials, bigquery_client, GBQ_query, job_config=job_config).result()
     return query_output
 
-def load_drywall_weights(walls_3d_JSON, polygons_JSON, compute_waste_average_standard=False, drywall_templates=None):
+def load_drywall_weights(walls_2d_JSON, polygons_JSON, compute_waste_average_standard=False, drywall_templates=None):
     weights_drywall = defaultdict(lambda: 0)
     drywall_count = 0
     waste_factor_total = 0
-    for wall in walls_3d_JSON:
-        for drywall in wall["surfaces_drywall"]:
+    for wall in walls_2d_JSON:
+        for drywall in wall["polygons_drywall"]:
             if not drywall["enabled"]:
                 continue
             if drywall["type_stacked"]:
@@ -745,15 +745,15 @@ def load_drywall_weights(walls_3d_JSON, polygons_JSON, compute_waste_average_sta
                 drywall_count += 1
                 weights_drywall[drywall["type"]] += 1
     for polygon in polygons_JSON:
-        if not polygon["surface_drywall"]["enabled"] or polygon["surface_drywall"]["type"] == "DISABLED":
+        if not polygon["polygon_drywall"]["enabled"] or polygon["polygon_drywall"]["type"] == "DISABLED":
             continue
-        drywall_template = query_drywall(polygon["surface_drywall"]["type"], drywall_templates)
+        drywall_template = query_drywall(polygon["polygon_drywall"]["type"], drywall_templates)
         if not drywall_template:
             continue
         if compute_waste_average_standard:
             waste_factor_total += float(drywall_template["waste"])
         drywall_count += 1
-        weights_drywall[polygon["surface_drywall"]["type"]] += 1
+        weights_drywall[polygon["polygon_drywall"]["type"]] += 1
     for drywall_type in weights_drywall.keys():
         weights_drywall[drywall_type] /= drywall_count
 
