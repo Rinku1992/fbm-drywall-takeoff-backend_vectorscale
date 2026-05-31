cff7aff43089ad1840f2b7b5d32f4d5615b6ce73
Debajyoti-R <debajyoti.r@myfbm.com>
2026-05-31 09:53:52 +0530

    Update helper.py

--- drywall-takeoff-3d/helper.py
+++ drywall-takeoff-3d/helper.py
@@ -1127,7 +1127,8 @@ async def load_visual_grounding(
         pages_metadata_filtered.append(page_metadata)
     n_pages = len(pages_metadata_filtered)
     page_batches = [list(map(lambda page_metadata: page_metadata["page_number"], pages_metadata_filtered[batch_index * batch_size: batch_index * batch_size + batch_size])) for batch_index in range(n_pages // batch_size)]
-    page_batches += [list(map(lambda page_metadata: page_metadata["page_number"], pages_metadata_filtered[n_pages - (n_pages % batch_size): n_pages]))]
+    if n_pages % batch_size != 0:
+        page_batches += [list(map(lambda page_metadata: page_metadata["page_number"], pages_metadata_filtered[n_pages - (n_pages % batch_size): n_pages]))]
 
     plan_paths = dict()
     for page_metadata in pages_metadata_filtered:
