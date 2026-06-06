4ec844da26cf60882680504662d4f7fab87a3d4f
Debajyoti-R <debajyoti.r@myfbm.com>
2026-06-06 23:21:26 +0530

    Update floor_plan.py

--- drywall-takeoff-3d/floor_plan.py
+++ drywall-takeoff-3d/floor_plan.py
@@ -265,37 +265,14 @@ class FloorPlan:
 
         return disconnected_shapes
 
-    def direction_polygon_interior(self, coordinates, wall_line):
-        X1, Y1, X2, Y2 = wall_line[0]
-        orientation = self.classify_line(X1, Y1, X2, Y2)
-        if orientation == "horizontal":
-            centroid_wall_line = (round((X1 + X2) / 2), round(np.median([Y1, Y2])))
-            if self.is_inside_polygon((centroid_wall_line[0], centroid_wall_line[1] - 50), coordinates):
-                return 'a'
-            return 'b'
-        if orientation == "vertical":
-            centroid_wall_line = (round(np.median([X1, X2])), round((Y1 + Y2) / 2))
-            if self.is_inside_polygon((centroid_wall_line[0] - 50, centroid_wall_line[1]), coordinates):
-                return 'a'
-            return 'b'
-        if orientation == "inclined":
-            dx = X2 - X1
-            dy = Y2 - Y1
-            length = math.hypot(dx, dy)
-
-            nx = -dy / length
-            ny =  dx / length
-
-            mx = (X1 + X2) / 2
-            my = (Y1 + Y2) / 2
-
-            test_coordinate = (
-                round(mx + nx * 50),
-                round(my + ny * 50)
-            )
-            if self.is_inside_polygon(test_coordinate, coordinates):
-                return 'a'
-            return 'b'
+    def direction_polygon_interior(self, coordinates, wall_2d):
+        polygon_centroid_X_0 = round(sum([coordinate['x'] for coordinate in wall_2d["polygons_drywall"][0]["polygon"]]) / 4)
+        polygon_centroid_Y_0 = round(sum([coordinate['y'] for coordinate in wall_2d["polygons_drywall"][0]["polygon"]]) / 4)
+        if self.is_inside_polygon((polygon_centroid_X_0, polygon_centroid_Y_0), coordinates):
+            wall_2d["polygons_drywall"][0]["id"] = f"{wall_2d["id"]}.a"
+            return 'a'
+        wall_2d["polygons_drywall"][1]["id"] = f"{wall_2d["id"]}.b"
+        return 'b'
 
     def load_perimeter(self, coordinates, wall_lines, tolerance=10, bound_capture=True, scale=None):
         tolerance_x, tolerance_y = tolerance, tolerance
